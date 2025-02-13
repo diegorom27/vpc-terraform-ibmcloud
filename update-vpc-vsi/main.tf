27234c29a04f46d974446feb08b8dc3320539f05
diegorom27 <diegorom27@gmail.com>
2025-02-13 15:27:33 -0500

    .

---
+++ update-vpc-vsi/main.tf
@@ -0,0 +1,41 @@
+##############################################################################
+# Terraform Providers
+##############################################################################
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = ">=1.19.0"
+    }
+  }
+}
+##############################################################################
+# Provider
+##############################################################################
+# ibmcloud_api_key = var.ibmcloud_api_key
+provider ibm {
+    alias  = "primary"
+    region = var.ibm_region
+    max_retries = 20
+}
+
+##############################################################################
+# Virtual Server Instance list
+##############################################################################
+
+data "ibm_is_instances" "ds_instances" {
+  resource_group = var.RESOURCE_GROUP
+}
+
+##############################################################################
+# Virtual Server Instance
+##############################################################################
+
+resource "ibm_is_instance" "vsi" {
+  for_each = { for vm in var.MACHINES : vm.name => vm }
+  name    =  data.ibm_is_instances.ds_instances[each.value.name]
+  profile = var.ENABLE_HIGH_PERFORMANCE ?each.value.hProfile:each.value.lProfile
+  image   = data.ibm_is_instances.ds_instances[each.value.name].image
+  vpc = data.ibm_is_instances.ds_instances[each.value.name].vpc
+  zone = data.ibm_is_instances.ds_instances[each.value.name].zone
+}
---
+++ update-vpc-vsi/vars.tf
@@ -0,0 +1,47 @@
+variable ibm_region {
+    description = "IBM Cloud region where all resources will be deployed"
+    type        = string
+    default = "us-south"
+    validation  {
+      error_message = "Must use an IBM Cloud region. Use `ibmcloud regions` with the IBM Cloud CLI to see valid regions."
+      condition     = can(
+        contains([
+          "au-syd",
+          "jp-tok",
+          "eu-de",
+          "eu-gb",
+          "us-south",
+          "us-east"
+        ], var.ibm_region)
+      )
+    }
+}
+variable RESOURCE_GROUP {
+    type        = string
+    default     = "test-amado"
+  
+}
+variable ENABLE_HIGH_PERFORMANCE {
+    type        = bool
+    default     = false
+}
+
+variable MACHINES {
+    description = "List of vm for control plane"
+    type = list(object({
+        name = string,
+        hProfile= string,
+        lProfile= string,
+    }))
+    default = [
+        {
+          name = "test1",
+          hProfile = "mx2-2x16",
+          lProfile = "cx2-2x4",
+        },{
+          name = "test2",
+          hProfile = "bx2-4x16",
+          lProfile = "cx2-2x4",
+        }
+    ]
+}
