7341b125c20e40650b9e8d8c2108fd1873e992d5
diegorom27 <diegorom27@gmail.com>
2025-02-17 16:38:16 -0500

    Update main.tf

--- update-vpc-vsi/main.tf
+++ update-vpc-vsi/main.tf
@@ -43,8 +43,8 @@ locals {
   terraform_state = jsondecode(file("${path.module}/terraform.tfstate"))
 
   ibm_instances_map = { for res in local.terraform_state.resources :
-    res.instances[0].attributes.id => res.instances[0].attributes
-    if res.type == "ibm_is_instance" 
+    res.instances.attributes.id => res.instances.attributes
+    if res.type == "ibm_is_instance"  && res.instances.attributes.resource_group == data.ibm_resource_group.group.id
   }
   unmanaged_instances_map = { for instance in data.ibm_is_instances.ds_instances.instances :
     instance.id => instance if lookup(local.ibm_instances_map, instance.id, null) == null
