6b49791a92f572cc947e1c3f466bb54fdd2def52
diegorom27 <diegorom27@gmail.com>
2025-02-17 16:19:17 -0500

    Update main.tf

--- update-vpc-vsi/main.tf
+++ update-vpc-vsi/main.tf
@@ -44,14 +44,14 @@ locals {
 
   ibm_instances_map = { for res in local.terraform_state.resources :
     res.instances[0].attributes.id => res.instances[0].attributes
-    if res.type == "ibm_is_instance"
+    if res.type == "ibm_is_instance" && res.instances[0].attributes.resource_group == data.ibm_resource_group.group.id
   }
   unmanaged_instances_map = { for instance in data.ibm_is_instances.ds_instances.instances :
     instance.id => instance if lookup(local.ibm_instances_map, instance.id, null) == null
   }
 }
 
-output " all_machinnes_state" {
+output "all_machinnes_state" {
   value = local.ibm_instances_map
 }
 
