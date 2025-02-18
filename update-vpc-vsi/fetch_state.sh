c6f1f76b4bb918e951d906f2c336d51e0a4c9ab6
diegorom27 <diegorom27@gmail.com>
2025-02-17 21:50:30 -0500

    Update fetch_state.sh

--- update-vpc-vsi/fetch_state.sh
+++ update-vpc-vsi/fetch_state.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Ejecuta el comando y formatea la salida en JSON
-output=$(ibmcloud schematics state list --id us-south.workspace.vpc-test.643cd01d --output json)
+output=$(ibmcloud schematics state list --id us-south.workspace.vpc-test.643cd01d --output json | jq '[.[] | select(.resources != null) | .resources[] | {resource_type, resource_name, resource_id, resource_group_name}]')
 
 # Si la salida está vacía, asignar un mapa vacío
 if [[ "$output" == "[]" ]]; then
