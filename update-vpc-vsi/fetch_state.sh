e1bbc0be3098fd328a761ce0d8d14824e7e0706f
diegorom27 <diegorom27@gmail.com>
2025-02-17 21:45:01 -0500

    Update fetch_state.sh

--- update-vpc-vsi/fetch_state.sh
+++ update-vpc-vsi/fetch_state.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# Exit if any of the intermediate steps fail
+set -e
+
 # Ejecuta el comando y formatea la salida en JSON
-output=$(ibmcloud schematics state list --id us-south.workspace.vpc-test.643cd01d --output json | jq '[.[] | select(.resources != null) | .resources[] | {resource_type, resource_name, resource_id, resource_group_name}]')
+output=$(ibmcloud schematics state list --id us-south.workspace.vpc-test.643cd01d --output json)
+
 # Si la salida está vacía, asignar un mapa vacío
 if [[ "$output" == "[]" ]]; then
   json_output="{}"
 else
-  # Convertir la salida en un mapa clave-valor adecuado para Terraform
-  json_output=$(echo "$output" | jq 'map({(.resource_id): .resource_name}) | add')
+  # Procesa los recursos para crear un mapa clave-valor con resource_id como clave
+  json_output=$(echo "$output" | jq 'map({(.resource_id): {resource_type: .resource_type, resource_name: .resource_name, resource_group_name: .resource_group_name}}) | add')
 fi
 
-# Imprime la salida como un objeto JSON
-echo "{\"state\": $json_output}"
+# Genera un JSON de salida
+jq -n --argjson state "$json_output" '{"state": $state}'
