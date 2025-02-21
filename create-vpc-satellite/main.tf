116d3b5c26fb9ab0b97ff2af5eb667c6f9485a46
diegorom27 <diegorom27@gmail.com>
2025-02-21 17:48:52 -0500

    Update main.tf

--- create-vpc-satellite/main.tf
+++ create-vpc-satellite/main.tf
@@ -331,3 +331,7 @@ resource "ibm_is_floating_ip" "fip_bastion" {
 output "fip_bastion" {
   value = ibm_is_floating_ip.fip_bastion.address
 }
+
+output "bastion_password_command" {
+  value = "ibmcloud is instance-initialization-values ${ibm_is_instance.bastion.id} --private-key "@~/.ssh/id_rsa""
+}
