421810394913e17b89d71aa68147a42ca2f3fb0c
diegorom27 <diegorom27@gmail.com>
2025-02-22 10:21:11 -0500

    Update main.tf

--- create-vpc-satellite/main.tf
+++ create-vpc-satellite/main.tf
@@ -51,7 +51,7 @@ locals {
 resource "ibm_satellite_location" "satellite-location-demo" {
   location          = "satellite"
   zones             = local.location_zones
-  managed_from      = var.ibm_region
+  managed_from      = "us-south-1"
   resource_group_id = data.ibm_resource_group.cluster-rg.id
   coreos_enabled   = false
 }
